b19c21af6e5e67288210e15a423baf054f888c7f
Marcin Wierzbiński <marcin119a@gmail.com>
2020-09-25 10:29:03 +0200

    2-MAX-SAT formulate as Ising Example

--- experiment.py
+++ experiment.py
@@ -1,5 +1,5 @@
 from formulaction import data_to_matrix, ising_params
-
+import ising
 
 cnf = [[1,2], [2,3], [1,-2],[2,-1]]
 
@@ -9,4 +9,16 @@ m = len(cnf)
 matrix = data_to_matrix(m, n, cnf)
 
 J,h = ising_params(m, n, matrix)
-print(J)
+
+graph  = { (i,i): k for i, k in enumerate(h) }
+
+
+J2 = {}
+for i, x in enumerate(J):
+    for j, m in enumerate(x):
+        if m !=0:
+            graph[(i,j)] = m
+
+
+result = ising.search(graph, num_states=8)
+print(result.energies)
